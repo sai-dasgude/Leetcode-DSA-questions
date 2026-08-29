791282cbb16792266112b4611012eea3ff9477ac
saidasgude11work-sudo <saidasgude11.work@gmail.com>
2026-08-29 10:42:20 +0530

    Time: 6 ms (5.9%), Space: 272.8 MB (6.69%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n  = nums.length;
+          if(k>n){
+            k = k % n;
+          }
+
+          int[] ans = new int[n];
+          int j = 0;
+          for(int i=(n-k)  ; i<n ;i++){
+               ans[j] = nums[i];
+               j++;
+        }
+        for(int i=0;i<(n-k);i++){
+            ans[j] = nums[i];
+            j++;
+        }
+
+        for(int i=0;i<n;i++){
+            nums[i] = ans[i];
+        }
+
+
+    }
+}
