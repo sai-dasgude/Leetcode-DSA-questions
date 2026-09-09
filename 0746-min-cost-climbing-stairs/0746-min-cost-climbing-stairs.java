fcbc13db8124b065400c26ba93217d0c570f0cf2
Sai Dasgude <saidasgude11.work@gmail.com>
2026-09-09 21:30:57 +0530

    Time: 1 ms (39.7%), Space: 45 MB (22.59%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+class Solution {
+
+    private int[] dp;
+    private int[] cost;
+
+    public int minCostClimbingStairs(int[] cost) {
+        this.cost = cost;
+
+        int n = cost.length;
+        dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+
+        return solve(n);
+    }
+
+    private int solve(int index) {
+        // We can start from stair 0 or stair 1
+        if (index <= 1) {
+            return 0;
+        }
+
+        // Answer already calculated
+        if (dp[index] != -1) {
+            return dp[index];
+        }
+
+        int oneStep = solve(index - 1) + cost[index - 1];
+        int twoSteps = solve(index - 2) + cost[index - 2];
+
+        return dp[index] = Math.min(oneStep, twoSteps);
+    }
+}
